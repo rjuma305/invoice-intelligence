3c5e51c4c5ef17922c1773e71d595654faade942
rjuma305 <robert.jumalon@yahoo.com>
2025-04-24 15:17:36 -0700

    Create InvoiceUploader.tsx

---
+++ client/src/lib/src/components/InvoiceUploader.tsx
@@ -0,0 +1,13 @@
+// Placeholder component for invoice upload functionality
+import React from 'react';
+
+const InvoiceUploader = () => {
+  return (
+    <div>
+      <h2>Upload Invoice</h2>
+      <input type="file" />
+    </div>
+  );
+};
+
+export default InvoiceUploader;
