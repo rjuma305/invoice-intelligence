7d27bedb7612fd6f8dd69e75ea3c70520b75b441
rjuma305 <robert.jumalon@yahoo.com>
2025-04-24 15:18:45 -0700

    Create InsightsDashboard.tsx

---
+++ client/src/lib/src/components/InsightsDashboard.tsx
@@ -0,0 +1,13 @@
+// Placeholder component for displaying insights
+import React from 'react';
+
+const InsightsDashboard = () => {
+  return (
+    <div>
+      <h2>Insights Dashboard</h2>
+      <p>Insights will be displayed here.</p>
+    </div>
+  );
+};
+
+export default InsightsDashboard;
